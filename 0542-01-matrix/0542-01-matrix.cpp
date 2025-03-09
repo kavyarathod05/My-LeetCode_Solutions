0a28bfe237421609e2bf1a0c4d4ef52835c7e390
kavya <149292618+kavyarathod05@users.noreply.github.com>
2025-03-09 19:04:15 +0530

    Time: 26 ms (35.92%), Space: 34.7 MB (84.05%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+        //process all 0s
+        queue<pair<int,int>> q;
+        vector<vector<int>>res(m,vector<int>(n,-1));
+        vector<pair<int, int>> dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; 
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==0){
+                q.push({i,j});
+                res[i][j]=0;
+                }
+            }
+        }
+        //multi source bfs
+        while(!q.empty()){
+            int i= q.front().first;
+            int j = q.front().second;
+            q.pop();
+            for(int k=0;k<4;k++){
+                int nx = i+dir[k].first;
+                int ny = j+ dir[k].second;
+                if(nx>=0 && ny>=0 && nx< m && ny<n && res[nx][ny]==-1){
+                    res[nx][ny]= res[i][j]+1;
+                    q.push({nx,ny});
+                }
+            }            
+        }
+        return res;
+    }
+};
