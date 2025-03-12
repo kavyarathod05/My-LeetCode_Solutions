8de87852c290681b28a05bda3d43e54dc6b1363a
kavya <149292618+kavyarathod05@users.noreply.github.com>
2025-03-12 15:42:39 +0530

    Time: 0 ms (100%), Space: 10.1 MB (89.65%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
-    int f(vector<int>&nums, int ind,vector<int>&dp){
-       int n = nums.size();
-       if(ind<0) return 0;
-       if(ind==0) return nums[0];
-       int pick = INT_MIN;
-       if(dp[ind]!=-1) return dp[ind];
-     
-       pick = nums[ind]+f(nums,ind-2,dp);
-       
-      int notpick = 0+ f(nums,ind-1,dp);
-        return dp[ind]=max(pick,notpick);
-    }
+   
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n,-1);
         if (n == 1) return nums[0]; 
-        dp[0]=nums[0];
-        dp[1] = max(nums[0], nums[1]);
-
+        int prev2=nums[0];
+        int prev1 = max(nums[0], nums[1]);
+        int curr=0;
         for(int i=2;i<n;i++){
-            int pick= INT_MIN;
-            if(i>1){
-             pick= nums[i]+dp[i-2];
-            }
-            int nonpick= 0+dp[i-1];
-            dp[i]= max(pick,nonpick);
-
+            
+            int pick= nums[i]+prev2;
+            int nonpick= 0+prev1;
+            curr= max(pick,nonpick);
+            prev2=prev1;
+            prev1=curr;
         }
-        return dp[n-1];
+        return prev1;
     }
 };
