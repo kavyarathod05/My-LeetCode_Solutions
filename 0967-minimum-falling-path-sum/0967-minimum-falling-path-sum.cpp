019d248be872af2500b0d96232bcb5b8a5d40859
kavya <149292618+kavyarathod05@users.noreply.github.com>
2025-03-17 10:30:41 +0530

    Time: 0 ms (100%), Space: 14.9 MB (75.1%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,23 +1,30 @@
-class Solution{
+class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        int ans=INT_MAX;
-         vector<vector<int>> dp(n,vector<int>(n,INT_MIN));
-        for(int i=0;i<n;i++){
-            ans=min(ans,f(matrix,0,i,n,dp));
+        int n = matrix.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        for(int j=0; j<n; j++){
+            dp[0][j] = matrix[0][j];
         }
+
+        for(int i=1; i<n; i++){
+            for(int j=0; j<n; j++){
+                int up = matrix[i][j] + dp[i-1][j];
+                int leftDiag = INT_MAX;
+                if(j-1 >= 0) leftDiag = matrix[i][j] + dp[i-1][j-1];
+                int rightDiag = INT_MAX;
+                if(j+1 < n) rightDiag = matrix[i][j] + dp[i-1][j+1];
+
+                dp[i][j] = min({up, leftDiag, rightDiag});
+            }
+        }
+
+        int ans = INT_MAX;
+        for(int j=0; j<n; j++){
+            ans = min(ans, dp[n-1][j]);
+        }
+
         return ans;
-    }
-private:
-    int f(vector<vector<int>>& matrix,int i,int j,int n,vector<vector<int>>&dp){
-        if(j<0) return INT_MAX;
-        if(i>=n || j>=n) return INT_MAX;
-        if(i==n-1) return matrix[i][j];
-        if(dp[i][j]!=INT_MIN) return dp[i][j];
-        int a=f(matrix,i+1,j-1,n,dp);
-        int b=f(matrix,i+1,j,n,dp);
-        int c=f(matrix,i+1,j+1,n,dp);
-        return dp[i][j]= min(a,min(b,c))+matrix[i][j];
     }
 };
