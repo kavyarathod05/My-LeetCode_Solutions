b1edf9b1e42bb31be59a11db41377fb6071d044d
kavya <149292618+kavyarathod05@users.noreply.github.com>
2025-05-13 17:53:31 +0530

    Time: 7 ms (9.56%), Space: 14.4 MB (91.24%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -2,29 +2,23 @@ class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
+        vector<int> prev(n, 0), curr(n, 0);
 
-        for(int j=0; j<n; j++){
-            dp[0][j] = matrix[0][j];
+        for (int j = 0; j < n; j++) {
+            prev[j] = matrix[0][j];
         }
 
-        for(int i=1; i<n; i++){
-            for(int j=0; j<n; j++){
-                int up = matrix[i][j] + dp[i-1][j];
-                int leftDiag = INT_MAX;
-                if(j-1 >= 0) leftDiag = matrix[i][j] + dp[i-1][j-1];
-                int rightDiag = INT_MAX;
-                if(j+1 < n) rightDiag = matrix[i][j] + dp[i-1][j+1];
-
-                dp[i][j] = min({up, leftDiag, rightDiag});
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int up = matrix[i][j] + prev[j];
+                int leftDiag = (j > 0) ? matrix[i][j] + prev[j - 1] : INT_MAX;
+                int rightDiag = (j < n - 1) ? matrix[i][j] + prev[j + 1] : INT_MAX;
+                
+                curr[j] = min({up, leftDiag, rightDiag});
             }
+            prev = curr;
         }
 
-        int ans = INT_MAX;
-        for(int j=0; j<n; j++){
-            ans = min(ans, dp[n-1][j]);
-        }
-
-        return ans;
+        return *min_element(prev.begin(), prev.end());
     }
 };
