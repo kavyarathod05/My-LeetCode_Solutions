28677ba2f53aaea78ec757cf324a04a4d0e97430
kavya <149292618+kavyarathod05@users.noreply.github.com>
2025-05-17 15:47:04 +0530

    Time: 12 ms (71.44%), Space: 17 MB (48.31%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -2,36 +2,35 @@ class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
         vector<vector<int>>mat(n,vector<int>(n,INT_MAX));
-        for(int i=0;i<edges.size();i++){
-            auto it=edges[i];
+        for(auto it:edges){
             mat[it[0]][it[1]]=it[2];
-            mat[it[1]][it[0]]=it[2];
+            mat[it[1]][it[0]] =it[2];
         }
         for(int i=0;i<n;i++){
             mat[i][i]=0;
         }
-        for (int k = 0; k < n; ++k) {
-            for (int i = 0; i < n; ++i) {
-                for (int j = 0; j < n; ++j) {
-                    if(mat[i][k]!=INT_MAX && mat[k][j]!=INT_MAX){
-                        mat[i][j]=min(mat[i][k]+mat[k][j],mat[i][j]);
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(mat[i][k]!= INT_MAX && mat[k][j]!= INT_MAX){
+                        mat[i][j]= min(mat[i][j],mat[i][k]+mat[k][j]);
                     }
                 }
             }
         }
-        int mini = INT_MAX;
+        int mini= INT_MAX;
         int best=-1;
         for(int i=0;i<n;i++){
-            int reach=0;
+            int reach =0;
             for(int j=0;j<n;j++){
                 if(mat[i][j]<=distanceThreshold){
                     reach++;
                 }
             }
-            if(reach<=mini){
-                mini=reach;
-                best=i;
-            }
+                if(reach<=mini){
+                    mini= reach;
+                    best=i;
+                }
         }
         return best;
     }
