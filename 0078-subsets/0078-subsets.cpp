c2368796a236a6b0f5dc2350f6b477af08dd9c32
kavya <149292618+kavyarathod05@users.noreply.github.com>
2025-06-13 16:02:22 +0530

    Time: 1 ms (47.23%), Space: 9.8 MB (81.11%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    void solve( vector<int>&nums, vector<int>& temp, vector<vector<int>>&ans,int ind){
-        int n = nums.size();
-        if(ind==n) {
-            ans.push_back(temp);
-            return;
-        }
-        temp.push_back(nums[ind]);
-        solve(nums, temp ,ans , ind+1);// take part
-        temp.pop_back(); // remove last element as to perform take/not take
-        solve(nums,temp ,ans, ind+1); //Not take part
-
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
         int n = nums.size();
-        vector<int> temp;
-        vector<vector<int>>ans;
-        solve(nums,temp ,ans, 0);
+        vector<vector<int>> ans;
+        int total = 1 << n; 
+
+        for(int i = 0; i < total; i++){
+            vector<int> temp;
+            for(int j = 0; j < n; j++){
+                if(i & (1 << j)){
+                    temp.push_back(nums[j]);
+                }
+            }
+            ans.push_back(temp);
+        }
         return ans;
     }
 };
