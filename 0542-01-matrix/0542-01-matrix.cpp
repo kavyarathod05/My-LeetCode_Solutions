b9ed49f79b2b27164dcfa57f450fb031b1c5df31
kavya <149292618+kavyarathod05@users.noreply.github.com>
2025-06-27 17:56:42 +0530

    Time: 3 ms (98.85%), Space: 34.8 MB (78.94%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -2,32 +2,30 @@ class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int m = mat.size();
-        int n = mat[0].size();
-        //process all 0s
-        queue<pair<int,int>> q;
-        vector<vector<int>>res(m,vector<int>(n,-1));
+        int n  =mat[0].size();
+        queue<pair<int,int>>q;
+        vector<vector<int>> res(m,vector<int>(n,-1));
         vector<pair<int, int>> dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(mat[i][j]==0){
-                q.push({i,j});
-                res[i][j]=0;
+                    res[i][j]=0;
+                    q.push({i,j});
                 }
             }
         }
-        //multi source bfs
         while(!q.empty()){
-            int i= q.front().first;
+            int i = q.front().first;
             int j = q.front().second;
             q.pop();
             for(int k=0;k<4;k++){
-                int nx = i+dir[k].first;
+                int nx= i+ dir[k].first;
                 int ny = j+ dir[k].second;
-                if(nx>=0 && ny>=0 && nx< m && ny<n && res[nx][ny]==-1){
-                    res[nx][ny]= res[i][j]+1;
+                if(nx>=0 && ny>=0 && nx<m && ny<n && res[nx][ny]==-1){
+                    res[nx][ny] = 1+ res[i][j];
                     q.push({nx,ny});
                 }
-            }            
+            }
         }
         return res;
     }
