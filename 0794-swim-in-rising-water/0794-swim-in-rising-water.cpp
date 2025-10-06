8595a7145f9dc5bb65e3554c5246839a08316799
kavya <149292618+kavyarathod05@users.noreply.github.com>
2025-10-06 15:12:30 +0530

    Time: 8 ms (50.24%), Space: 15.6 MB (19.88%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -1,42 +1,53 @@
 class Solution {
 public:
-    int n;
-    vector<vector<int>> dirs{{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-    bool canReach(int mid, vector<vector<int>>& grid, int i, int j,
-                  vector<vector<bool>>& vis) {
-        if (i < 0 || i >= n || j < 0 || j >= n || vis[i][j] ||
-            grid[i][j] > mid)
-            return false;
-        if (i == n - 1 && j == n - 1)
-            return true;
-        vis[i][j]=true;
-        for(auto &d:dirs){
-            int ni= i+d[0], nj= j+d[1];
-            if(canReach(mid, grid, ni ,nj, vis)) return true;
-        }
-        return false;
+    bool check(int mid, vector<vector<int>>& grid){
+        int n = grid.size();
+        if(grid[0][0] > mid) return false;
 
+        queue<pair<int,int>> q;
+        vector<vector<int>> vis(n, vector<int>(n,0));
 
-    }
-    int swimInWater(vector<vector<int>>& grid) {
-        n = grid.size();
-        int low = grid[0][0];
-        int high = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                high = max(high, grid[i][j]);
+        q.push({0,0});
+        vis[0][0] = 1;
+
+        vector<pair<int, int>> dirs= {{0,1}, {1,0}, {-1,0}, {0,-1}};
+        while(!q.empty()){
+            auto [x, y] = q.front();
+            q.pop();
+
+            if(x == n-1 && y == n-1) return true;
+
+            for(auto [dx, dy] : dirs){
+                int nx = x + dx, ny = y + dy;
+                if(nx>=0 && ny>=0 && nx<n && ny<n 
+                   && !vis[nx][ny] && grid[nx][ny] <= mid){
+                    vis[nx][ny] = 1;
+                    q.push({nx, ny});
+                }
             }
         }
-        while (low < high) {
-            int mid = (low + high) / 2;
-            vector<vector<bool>> vis(n, vector<bool>(n, false));
+        return false;
+    }
 
-            if (canReach(mid, grid, 0, 0, vis)) {
-                high = mid;
+    int swimInWater(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int maxi = INT_MIN;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                maxi = max(maxi, grid[i][j]);
+            }
+        }
+
+        int low = 0, high = maxi, ans = maxi;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(check(mid, grid)){
+                ans = mid;
+                high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
-        return low;
+        return ans;
     }
 };
