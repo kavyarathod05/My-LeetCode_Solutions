594aa80b839edcf6a8d9b68793ae2a4a4b389cb0
kavya <149292618+kavyarathod05@users.noreply.github.com>
2025-10-19 11:14:50 +0530

    Time: 3 ms (65.33%), Space: 10.2 MB (53.82%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,57 +1,55 @@
 class Solution {
 public:
-    bool isSafe(int board[][10], int i, int j, int n) {
-        for(int row = 0; row < i; row++) {
-            if(board[row][j] == 1) return false;
+    bool isSafe(int row ,int col , vector<string>&curr){
+        if(row<0 || col <0 || row>curr.size() || col> curr.size()) return false;
+        int drow= row;
+        int dcol = col;
+        
+        while(row>=0 && col>=0){
+            if(curr[row][col]=='Q') return false;
+            row--;
+            col--;
         }
+        
+        row= drow;
+        col = dcol;
 
-        int x = i, y = j;
-        while(x >= 0 && y >= 0) {
-            if(board[x][y] == 1) return false;
-            x--;
-            y--;
+        while(col>=0){
+            if(curr[row][col]=='Q') return false;
+            col--;
         }
-
-        x = i, y = j;
-        while(x >= 0 && y < n) {
-            if(board[x][y] == 1) return false;
-            x--;
-            y++;
+        row= drow;
+        col = dcol;
+        while(row<curr.size() && col>=0){
+            if(curr[row][col]=='Q') return false;
+            row++;
+            col--;
         }
-
         return true;
+
     }
-
-    bool NQueen(int board[][10], int i, int n, vector<vector<string>>& v) {
-        if(i == n) {
-            vector<string> curr;
-            for(int row = 0; row < n; row++) {
-                string rowStr;
-                for(int col = 0; col < n; col++) {
-                    if(board[row][col] == 1) rowStr += 'Q';
-                    else rowStr += '.';
-                }
-                curr.push_back(rowStr);
-            }
-            v.push_back(curr);
-            return false;
+    void f(int col, vector<vector<string>>&ans ,vector<string>&curr, int n){
+        if(col==n){
+            ans.push_back(curr);
+            return;
         }
-
-        for(int j = 0; j < n; j++) {
-            if(isSafe(board, i, j, n)) {
-                board[i][j] = 1;
-                bool NextQueen = NQueen(board, i + 1, n, v);
-                if(NextQueen == true) return true;
-                board[i][j] = 0;
+        for(int row=0;row<n;row++){
+            if(isSafe(row , col , curr)){
+                curr[row][col]='Q';
+                f(col+1 ,ans, curr, n);
+                curr[row][col]='.';
             }
         }
-        return false;
-    }
 
+    }
     vector<vector<string>> solveNQueens(int n) {
-        int board[10][10] = {0};
-        vector<vector<string>> v;
-        NQueen(board, 0, n, v);
-        return v;
+        vector<vector<string>> ans;
+        vector<string> curr(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++){
+            curr[i]=s;
+        }
+        f(0,  ans, curr, n);
+        return ans;
     }
 };
