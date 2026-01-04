af8517828fe62c0bdf403a3a8a260c75c1f728b3
kavya <149292618+kavyarathod05@users.noreply.github.com>
2026-01-05 00:26:41 +0530

    Time: 0 ms (100%), Space: 9 MB (26.15%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    void solve(int k, int n, vector<int>& nums, vector<int>& temp,
-               vector<vector<int>>& ans, int ind) {
-        if (k < 0 || n < 0) {
+    void f(int ind, vector<int>& nums, vector<vector<int>>& ans, vector<int>& temp, int k, int n) {
+        if (temp.size() == k) {
+            if (n == 0)
+                ans.push_back(temp);
             return;
         }
-        if (k == 0 && n == 0) {
-            ans.push_back(temp);
-            return;
-        }
-        for (int i = ind; i < nums.size(); i++) {
-            temp.push_back(nums[i]);
-            solve(k - 1, n - nums[i], nums, temp, ans, i + 1);
+        if (ind >= nums.size()) return;
+
+        f(ind + 1, nums, ans, temp, k, n);
+
+        if (nums[ind] <= n) {
+            temp.push_back(nums[ind]);
+            f(ind + 1, nums, ans, temp, k, n - nums[ind]);
             temp.pop_back();
         }
     }
+
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int> nums(9);
-        vector<int> temp;
-        for (int i = 0; i < 9; i++) {
-            nums[i] = i + 1;
-        }
+        vector<int> nums;
+        for (int i = 1; i <= 9; i++) nums.push_back(i);
         vector<vector<int>> ans;
-        solve(k, n, nums, temp, ans, 0);
+        vector<int> temp;
+        f(0, nums, ans, temp, k, n);
         return ans;
     }
 };
