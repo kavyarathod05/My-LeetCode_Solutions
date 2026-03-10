66985c88f01739501b814b32955a2cd0a48fa5ec
kavya <149292618+kavyarathod05@users.noreply.github.com>
2026-03-10 16:55:17 +0530

    Time: 470 ms (26.02%), Space: 51.1 MB (39.02%) - LeetHub

--- 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.cpp
+++ 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.cpp
@@ -1,58 +1,50 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    const int MOD = 1e9+7;
+    const int MOD = 1e9 + 7;
+    int limit;
+    
+    int solve(int z, int o, int last, vector<vector<vector<int>>>& dp) {
+        
+        if(z == 0 && o == 0)
+            return 1;
 
-    int numberOfStableArrays(int zero, int one, int limit) {
+        if(dp[z][o][last] != -1)
+            return dp[z][o][last];
 
-        static int dp[201][201][2][201];
-        memset(dp,0,sizeof(dp));
+        long long ans = 0;
 
-        if(zero>0) dp[1][0][0][1] = 1;
-        if(one>0) dp[0][1][1][1] = 1;
-
-        for(int i=0;i<=zero;i++){
-            for(int j=0;j<=one;j++){
-                for(int last=0;last<2;last++){
-                    for(int cnt=1;cnt<=limit;cnt++){
-
-                        int cur = dp[i][j][last][cnt];
-                        if(cur==0) continue;
-
-                        // place 0
-                        if(i+1<=zero){
-                            if(last==0){
-                                if(cnt+1<=limit)
-                                    dp[i+1][j][0][cnt+1] = (dp[i+1][j][0][cnt+1] + cur)%MOD;
-                            }
-                            else{
-                                dp[i+1][j][0][1] = (dp[i+1][j][0][1] + cur)%MOD;
-                            }
-                        }
-
-                        // place 1
-                        if(j+1<=one){
-                            if(last==1){
-                                if(cnt+1<=limit)
-                                    dp[i][j+1][1][cnt+1] = (dp[i][j+1][1][cnt+1] + cur)%MOD;
-                            }
-                            else{
-                                dp[i][j+1][1][1] = (dp[i][j+1][1][1] + cur)%MOD;
-                            }
-                        }
-
-                    }
-                }
+        if(last == 1) {  // last block was 1, so place zeros
+            for(int k = 1; k <= limit && z >= k; k++) {
+                ans = (ans + solve(z - k, o, 0, dp)) % MOD;
+            }
+        }
+        else { // last block was 0, so place ones
+            for(int k = 1; k <= limit && o >= k; k++) {
+                ans = (ans + solve(z, o - k, 1, dp)) % MOD;
             }
         }
 
-        long long ans=0;
+        return dp[z][o][last] = ans;
+    }
 
-        for(int last=0;last<2;last++)
-            for(int cnt=1;cnt<=limit;cnt++)
-                ans=(ans+dp[zero][one][last][cnt])%MOD;
+    int numberOfStableArrays(int zero, int one, int lim) {
+
+        limit = lim;
+
+        vector<vector<vector<int>>> dp(
+            zero + 1,
+            vector<vector<int>>(one + 1, vector<int>(2, -1))
+        );
+
+        long long ans = 0;
+
+        // start with zero block
+        for(int k = 1; k <= limit && zero >= k; k++)
+            ans = (ans + solve(zero - k, one, 0, dp)) % MOD;
+
+        // start with one block
+        for(int k = 1; k <= limit && one >= k; k++)
+            ans = (ans + solve(zero, one - k, 1, dp)) % MOD;
 
         return ans;
     }
