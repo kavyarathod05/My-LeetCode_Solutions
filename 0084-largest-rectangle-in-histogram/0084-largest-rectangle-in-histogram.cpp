adb379ebb896c46bdd32db394284dde79a2cad11
kavya <149292618+kavyarathod05@users.noreply.github.com>
2026-03-17 10:27:11 +0530

    Time: 28 ms (34.43%), Space: 85.7 MB (41.63%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
-    vector<int> Nse(vector<int>& heights){
-        int n = heights.size();
-        vector<int> ans(n,-1);
-        stack<int> st;
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && heights[i]<=heights[st.top()]){
-                st.pop();
-            }
-            ans[i]= st.empty()?n:st.top();
-            st.push(i);
-        }
-        return ans;
-    }
-    vector<int> Pse(vector<int>& heights){
-        int n = heights.size();
-        vector<int> ans(n,-1);
-        stack<int> st;
-        for(int i=0;i<n;i++){
-            while(!st.empty() && heights[i]<=heights[st.top()]){
-                st.pop();
-            }
-            ans[i]= st.empty()?-1:st.top();
-            st.push(i);
-        }
-        return ans;
-    }
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
-        vector<int> nse= Nse(heights);
-        vector<int> pse= Pse(heights);
-        int maxi= 0;
-        for(int i=0;i<n;i++){
-            int area= ((nse[i])-(pse[i])-1)*heights[i];
-            maxi= max(area, maxi);
+        vector<int> left(n), right(n);
+        stack<int> st;
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
+            }
+            left[i] = st.empty() ? -1 : st.top();
+            st.push(i);
         }
-        return maxi;
+        while (!st.empty()) st.pop();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
+            }
+            right[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int width = right[i] - left[i] - 1;
+            ans = max(ans, heights[i] * width);
+        }
+
+        return ans;
     }
 };
