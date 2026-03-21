57c62042c3111d47f0e7ebad0504a19daa8dec6b
kavya <149292618+kavyarathod05@users.noreply.github.com>
2026-03-21 18:22:04 +0530

    Time: 101 ms (70.44%), Space: 194.5 MB (41.37%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.cpp
+++ 0460-lfu-cache/0460-lfu-cache.cpp
@@ -1,55 +1,82 @@
+class Node {
+public:
+    int key, value, freq;
+    list<Node*>::iterator it;
+
+    Node(int k, int v) {
+        key = k;
+        value = v;
+        freq = 1;
+    }
+};
+
 class LFUCache {
 public:
-    int cap;
-    int size;
-    unordered_map<int,list<vector<int>>::iterator>mp;
-    map<int,list<vector<int>>>freq;
+    int capacity;
+    int minFreq;
+
+    unordered_map<int, Node*> mp;
+    unordered_map<int, list<Node*>> freqList;
+
     LFUCache(int capacity) {
-        cap=capacity;
-        size=0;
+        this->capacity = capacity;
+        minFreq = 0;
     }
 
-    void makeMostFrequentlyUsed(int k){
-        auto &vec=(*(mp[k]));
-        int key=vec[0],value=vec[1],f=vec[2];
+    void updateFreq(Node* node) {
+        int freq = node->freq;
 
-        freq[f].erase(mp[key]);
-        if(freq[f].empty()) freq.erase(f);
+        freqList[freq].erase(node->it);
 
-        freq[f+1].push_front(vector<int>({key,value,f+1}));
-        mp[key]=freq[f+1].begin();
-    }
-    
-    int get(int key) {
-        if(!mp.count(key)) return -1;
-
-        auto &vec=(*(mp[key]));
-        int val=vec[1];
-        makeMostFrequentlyUsed(key);
-        return val;
-    }
-    
-    void put(int key, int value) {
-        if(cap==0) return;
-
-        if(mp.count(key)){
-            auto &vec=(*(mp[key]));
-            vec[1]=value;
-            makeMostFrequentlyUsed(key);
-        } else if(size<cap){
-            size++;
-            freq[1].push_front(vector<int>({key,value,1}));
-            mp[key]=freq[1].begin();
-        } else{
-            auto &lru_list=freq.begin()->second;
-            int key_delete=lru_list.back()[0];
-            lru_list.pop_back();
-            mp.erase(key_delete);
-
-            if(lru_list.empty()) freq.erase(freq.begin()->first);
-
-            freq[1].push_front(vector<int>({key,value,1}));
-            mp[key]=freq[1].begin();
+        if (freqList[freq].empty()) {
+            freqList.erase(freq);
+            if (minFreq == freq) minFreq++;
         }
+
+        node->freq++;
+
+        freqList[node->freq].push_back(node);
+        node->it = --freqList[node->freq].end();
+    }
+
+    int get(int key) {
+        if (mp.find(key) == mp.end()) return -1;
+
+        Node* node = mp[key];
+        updateFreq(node);
+        return node->value;
+    }
+
+    void put(int key, int value) {
+        if (capacity == 0) return;
+
+        if (mp.find(key) != mp.end()) {
+            Node* node = mp[key];
+            node->value = value;
+            updateFreq(node);
+            return;
+        }
+
+        if (mp.size() == capacity) {
+            auto &list = freqList[minFreq];
+
+            Node* nodeToRemove = list.front();
+            list.pop_front();
+
+            mp.erase(nodeToRemove->key);
+            delete nodeToRemove;
+
+            if (list.empty()) {
+                freqList.erase(minFreq);
+            }
+        }
+
+        Node* newNode = new Node(key, value);
+        mp[key] = newNode;
+
+        freqList[1].push_back(newNode);
+        newNode->it = --freqList[1].end();
+
+        minFreq = 1;
     }
 };
