be619506a150ab57c19eab28af42dd8e7c5ad6ab
kavya <149292618+kavyarathod05@users.noreply.github.com>
2026-03-22 12:31:38 +0530

    Time: 39 ms (11.74%), Space: 34.7 MB (83.41%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,32 +1,40 @@
 class Solution {
 public:
+    vector<pair<int,int>> dirs = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int m = mat.size();
-        int n  =mat[0].size();
-        queue<pair<int,int>>q;
-        vector<vector<int>> res(m,vector<int>(n,-1));
-        vector<pair<int, int>> dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(mat[i][j]==0){
-                    res[i][j]=0;
-                    q.push({i,j});
+        int n = mat[0].size();
+
+        queue<pair<int,int>> q;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(mat[i][j] == 0){
+                    q.push({i, j});   
+                } else {
+                    mat[i][j] = INT_MAX;
                 }
             }
         }
+
         while(!q.empty()){
-            int i = q.front().first;
-            int j = q.front().second;
+            auto [x, y] = q.front();
             q.pop();
-            for(int k=0;k<4;k++){
-                int nx= i+ dir[k].first;
-                int ny = j+ dir[k].second;
-                if(nx>=0 && ny>=0 && nx<m && ny<n && res[nx][ny]==-1){
-                    res[nx][ny] = 1+ res[i][j];
-                    q.push({nx,ny});
+
+            for(auto& d : dirs){
+                int nx = x + d.first;
+                int ny = y + d.second;
+
+                if(nx >= 0 && ny >= 0 && nx < m && ny < n){
+                    if(mat[nx][ny] > mat[x][y] + 1){  
+                        mat[nx][ny] = mat[x][y] + 1;
+                        q.push({nx, ny});
+                    }
                 }
             }
         }
-        return res;
+
+        return mat;
     }
 };
