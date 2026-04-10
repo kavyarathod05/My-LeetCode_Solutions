decbe0ff51026f18f0fe76e6b2451361534d7459
kavya <149292618+kavyarathod05@users.noreply.github.com>
2026-04-10 11:11:54 +0530

    Time: 67 ms (9.04%), Space: 49.9 MB (17.62%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    int f(int ind, vector<int>& coins, int target,vector<vector<int>>&dp) {
-        if (target == 0) return 0; 
-        if (ind == 0) {
-            return (target % coins[0] == 0) ? (target / coins[0]) : 1e9;
-        }
-        if(dp[ind][target]!=-1) return dp[ind][target];
-        int notTake = f(ind - 1, coins, target,dp) ;
-        int take = 1e9;
-        
-        if (coins[ind] <= target) {
-            take = 1 + f(ind, coins, target - coins[ind],dp);
+    int f(int ind, int left, vector<int>& coins, vector<vector<int>>& dp) {
+        if (ind == coins.size()) {
+            if (left == 0) return 0;
+            return 1e9;
         }
 
-        return dp[ind][target]= min(take, notTake);
+        if (dp[ind][left] != -1) return dp[ind][left];
+
+        int notake = f(ind + 1, left, coins, dp);
+
+        int take = 1e9;
+        if (coins[ind] <= left) {
+            int res = f(ind, left - coins[ind], coins, dp);
+            if (res != 1e9) take = 1 + res;
+        }
+
+        return dp[ind][left] = min(take, notake);
     }
 
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
-        int ans = f(n - 1, coins, amount,dp);
-        return (ans >= 1e9) ? -1 : ans; 
+        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+
+        int ans = f(0, amount, coins, dp);
+        return (ans >= 1e9) ? -1 : ans;
     }
 };
