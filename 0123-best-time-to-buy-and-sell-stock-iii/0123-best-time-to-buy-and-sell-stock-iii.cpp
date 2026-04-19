cd3189ec7cbcd124b902f3f0ee11f295b0b580ce
kavya <149292618+kavyarathod05@users.noreply.github.com>
2026-04-19 16:44:52 +0530

    Time: 476 ms (10.14%), Space: 225.4 MB (11.65%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-    int f(int ind, int buy , int cap , vector<int>&prices, vector<vector<vector<int>>>&dp){
-        int n  = prices.size();
+    int f(int ind, int buy , int cap ,vector<int>& prices, vector<vector<vector<int>>>&dp){
+        int n = prices.size();
         if(ind==n || cap==0) return 0;
-        if(dp[ind][buy][cap] != -1){
-            return dp[ind][buy][cap];
+        if(dp[ind][buy][cap]!=-1) return dp[ind][buy][cap];
+        if(buy){
+            // then 2 options either i buy or not buy
+            return dp[ind][buy][cap]= max(-prices[ind]+f(ind+1,0,cap,prices ,dp),f(ind+1, buy, cap, prices,dp));
         }
-        int profit;
-      if (buy == 0) { 
-        profit = max(0 + f( ind + 1, 0, cap,prices, dp),
-                     -prices[ind] + f( ind + 1, 1, cap,prices, dp));
-    }
-
-    if (buy == 1) { 
-        profit = max(0 + f( ind + 1, 1, cap,prices, dp),
-                     prices[ind] + f( ind + 1, 0, cap - 1,prices, dp));
-    }
-
-    
-    return dp[ind][buy][cap] = profit;
+        else{
+            return dp[ind][buy][cap]=  max(prices[ind]+f(ind+1, 1, cap-1,prices,dp), f(ind+1, buy, cap, prices,dp));
+        }
     }
     int maxProfit(vector<int>& prices) {
+        // ind , buy/sell ,cap profit i want to maximise;
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
-        return f(0,0,2,prices,dp);
-
+        // for second one buy ==1 then you can buy
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3,-1)));
+        return f(0,1,2, prices,dp);
     }
 };
