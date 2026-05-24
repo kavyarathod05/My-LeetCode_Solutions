44ebf2a65b0e95af24448b0c3fcdaaca91d34982
kavya <149292618+kavyarathod05@users.noreply.github.com>
2026-05-24 18:34:42 +0530

    Time: 908 ms (12.12%), Space: 19.1 MB (95.91%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord,
-                     vector<string>& wordList) {
-        unordered_set<string> st;
-        for (auto& words : wordList) {
-            st.insert(words);
-        }
-        queue<pair<string,int>>q;
-        st.erase(beginWord);
-        q.push({beginWord, 1});
-        while (!q.empty()) {
-            auto it = q.front();
-            q.pop();
-            string s = it.first;
-            int dist = it.second;
-            if(endWord==s) return dist;
-            for (int i = 0; i < s.size(); i++) {
-                char ch = s[i];
-                for (char c = 'a'; c <= 'z'; c++) {
-                    s[i]=c;
-                    if(st.count(s)){
-                        q.push({s,dist+1});
-                        st.erase(s);
-                    }
-                }
-                s[i] = ch;
+    bool oneDiff(string a, string b){
+        int n = a.size();
+        int cnt= 0;
+        for(int i=0;i<n;i++){
+            if(a[i] != b[i]) {
+                 cnt++;
             }
         }
-        return 0;
+        return cnt==1;
+    }
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        int n = wordList.size();
+        queue<pair<string, int>> q;
+        vector<int> vis(n,0);
+        q.push({beginWord, 1});
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            string word = it.first;
+            int distance = it.second;
+            if(word == endWord) return distance;
+
+            for(int i=0;i<n;i++){
+                if(!vis[i] && oneDiff(word, wordList[i])){
+                    q.push({
+                        wordList[i],
+                        distance+1
+                    });
+                    vis[i]=1;
+                }
+            }
+        }
+        return 0; 
     }
 };
