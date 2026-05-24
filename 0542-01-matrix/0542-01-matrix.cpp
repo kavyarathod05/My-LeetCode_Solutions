dab0fd632c924ecfead2c3f3eae92951e4fd1088
kavya <149292618+kavyarathod05@users.noreply.github.com>
2026-05-24 17:45:12 +0530

    Time: 2030 ms (5%), Space: 31.7 MB (98.69%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,40 +1,36 @@
 class Solution {
 public:
-    vector<pair<int,int>> dirs = {{0,1}, {1,0}, {0,-1}, {-1,0}};
-
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+
         int m = mat.size();
         int n = mat[0].size();
 
-        queue<pair<int,int>> q;
+        vector<vector<int>> ans(m, vector<int>(n, 0));
 
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(mat[i][j] == 0){
-                    q.push({i, j});   
-                } else {
-                    mat[i][j] = INT_MAX;
-                }
-            }
-        }
+        for(int i = 0; i < m; i++) {
 
-        while(!q.empty()){
-            auto [x, y] = q.front();
-            q.pop();
+            for(int j = 0; j < n; j++) {
 
-            for(auto& d : dirs){
-                int nx = x + d.first;
-                int ny = y + d.second;
+                if(mat[i][j] == 1) {
 
-                if(nx >= 0 && ny >= 0 && nx < m && ny < n){
-                    if(mat[nx][ny] > mat[x][y] + 1){  
-                        mat[nx][ny] = mat[x][y] + 1;
-                        q.push({nx, ny});
+                    int mini = INT_MAX;
+
+                    for(int x = 0; x < m; x++) {
+
+                        for(int y = 0; y < n; y++) {
+
+                            if(mat[x][y] == 0) {
+                                mini = min(mini,
+                                           abs(i - x) + abs(j - y));
+                            }
+                        }
                     }
+
+                    ans[i][j] = mini;
                 }
             }
         }
 
-        return mat;
+        return ans;
     }
 };
