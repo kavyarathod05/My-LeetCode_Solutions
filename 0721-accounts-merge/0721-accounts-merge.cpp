43c333293ff44e0d2b6ef593faa3670a393bcd78
kavya <149292618+kavyarathod05@users.noreply.github.com>
2026-05-29 11:36:01 +0530

    Time: 34 ms (60%), Space: 42.4 MB (38.09%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,59 +1,52 @@
 class Solution {
 public:
-    int find(int node, vector<int>& par) {
-        if(par[node] == node) return node;
-        return par[node] = find(par[node], par);
-    }
-
-    bool unionset(int u, int v, vector<int>& par, vector<int>& sz) {
-        int pu = find(u, par);
-        int pv = find(v, par);
-        if(pu == pv) return false;
-        if(sz[pu] < sz[pv]) {
-            par[pu] = pv;
-            sz[pv] += sz[pu];
-        } else {
-            par[pv] = pu;
-            sz[pu] += sz[pv];
-        }
-        return true;
-    }
-
-    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        int n = accounts.size();
-        map<string, int> mailtonode;
-        vector<int> parent(n);
-        vector<int> size(n, 1);
-        iota(parent.begin(), parent.end(), 0);
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 1; j < accounts[i].size(); j++) {
-                string email = accounts[i][j];
-                if(mailtonode.find(email) == mailtonode.end()) {
-                    mailtonode[email] = i;
-                } else {
-                    unionset(i, mailtonode[email], parent, size);
-                }
+    void dfs(int node, vector<int> &comp , vector<int>&vis, vector<vector<int>>&adj){
+        vis[node] =1;
+        comp.push_back(node);
+        for(auto nbr : adj[node]){
+            if(!vis[nbr]){
+                dfs(nbr, comp , vis, adj);
             }
         }
+    }
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        unordered_map<string, int> emailToAccount;
+        int n = accounts.size();
 
-        map<int, vector<string>> nodemails;
-        for(auto& it : mailtonode) {
-            string email = it.first;
-            int node = it.second;
-            int root = find(node, parent);
-            nodemails[root].push_back(email);
-        }
-
+        vector<vector<int>> adj(n);
+        for(int i=0;i<n;i++){
+            for(int j = 1; j<accounts[i].size();j++){
+                string mail = accounts[i][j];
+                if(emailToAccount.find(mail) == emailToAccount.end()){
+                    emailToAccount[mail] = i;
+                }
+                else{
+                    int prev = emailToAccount[mail];
+                    adj[prev].push_back(i);
+                    adj[i].push_back(prev);
+                }
+            }
+        }   
+        vector<int> vis(n,0);
         vector<vector<string>> ans;
-        for(auto& it : nodemails) {
-            int node = it.first;
-            vector<string> emails = it.second;
-            sort(emails.begin(), emails.end());
-            emails.insert(emails.begin(), accounts[node][0]); // name at front
-            ans.push_back(emails);
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                vector<int> comp;
+                dfs(i, comp, vis ,adj);
+                set<string> mails;
+                for(auto acc : comp){
+                    for(int i=1 ; i< accounts[acc].size();i++){
+                        mails.insert(accounts[acc][i]);
+                    }
+                }
+                vector<string> merged;
+                merged.push_back(accounts[comp[0]][0]);
+                for(auto it:mails){
+                    merged.push_back(it);
+                }
+                ans.push_back(merged);
+            }
         }
-
         return ans;
     }
 };
