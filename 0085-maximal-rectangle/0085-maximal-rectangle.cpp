ec7e2a1c5f65cdf846a660af661b2814b4561f75
kavya <149292618+kavyarathod05@users.noreply.github.com>
2026-06-17 19:40:04 +0530

    Time: 3 ms (91.05%), Space: 18.6 MB (89.16%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
+        if(matrix.empty()) return 0;
+
         int m = matrix.size();
         int n = matrix[0].size();
-        
-        vector<vector<int>> grid(m, vector<int>(n,0));
+
+        vector<int> height(n, 0);
         int ans = 0;
 
-        for(int j = 0; j < n; j++){
-            if(matrix[0][j] == '1') grid[0][j] = 1;
-        }
+        for(int i = 0; i < m; i++) {
 
-        for(int i = 0; i < m; i++){
-            
-            if(i > 0){
-                for(int j = 0; j < n; j++){
-                    if(matrix[i][j] == '1'){
-                        grid[i][j] = grid[i-1][j] + 1;
-                    } else {
-                        grid[i][j] = 0;
-                    }
-                }
+            // Build histogram
+            for(int j = 0; j < n; j++) {
+                if(matrix[i][j] == '1')
+                    height[j]++;
+                else
+                    height[j] = 0;
             }
 
             stack<int> st;
-            for(int j = 0; j <= n; j++){
-                int h = (j == n ? 0 : grid[i][j]);
-
-                while(!st.empty() && grid[i][st.top()] > h){
-                    int height = grid[i][st.top()];
+            for(int j=0; j<=n;j++){
+                while(!st.empty() && (j==n || height[st.top()] >= height[j])){
+                    int heigh = height[st.top()];
                     st.pop();
-
-                    int width = st.empty() ? j : j - st.top() - 1;
-                    ans = max(ans, height * width);
+                    int nsl = st.empty()?-1:st.top();
+                    int nsr = j;
+                    int width = nsr-nsl-1;
+                    ans = max(ans, width*heigh);
                 }
                 st.push(j);
             }
         }
+
         return ans;
     }
 };
