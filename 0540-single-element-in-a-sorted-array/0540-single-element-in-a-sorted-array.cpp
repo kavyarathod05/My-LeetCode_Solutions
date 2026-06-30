daee04830a58c0a7e92d76a3a5016b1fe72d4eb6
kavya <149292618+kavyarathod05@users.noreply.github.com>
2026-06-30 11:29:41 +0530

    Time: 0 ms (100%), Space: 26.2 MB (68.55%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-// mid == mid+1 so that left side is safe move to right side
-// if not safe then move left 
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
-        int low = 0, high= n-1;
-        int ans = 0;
+        int ans= -1;
+
+        int low =0 ,high = n-1;
         while(low<=high){
             int mid = (low+high)/2;
             if(mid%2==1) mid--;
-            if(mid<n-1 && nums[mid] == nums[mid+1]) {
+            if( mid < n-1 && nums[mid]== nums[mid+1]){
                 low = mid+2;
-                // so element is on right
             }
             else{
-                ans = mid;
+                ans = nums[mid];
                 high = mid-2;
             }
         }
-        return nums[ans];
+        return ans;
     }
 };
-//  [1,1,2,3,3,4,4,8,8] 
-// low = 0 , high = 8 , mid = 4 so check if prev not equal also if prev equal then move left back 
-// 
