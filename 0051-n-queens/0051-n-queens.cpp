b33b100c034e653ae253da13e748ce74168fda6f
kavya <149292618+kavyarathod05@users.noreply.github.com>
2026-07-02 17:33:33 +0530

    Time: 3 ms (62%), Space: 10.2 MB (59.5%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,46 +1,55 @@
 class Solution {
 public:
-    bool isSafe(int row, int col, vector<string>& board) {
-        int n = board.size();
-
-        // check same row (left side)
-        for (int j = 0; j < col; j++) {
-            if (board[row][j] == 'Q') return false;
+    bool isSafe(int row ,int col , vector<string>&curr){
+        if(row<0 || col <0 || row>curr.size() || col> curr.size()) return false;
+        int drow= row;
+        int dcol = col;
+        
+        while(row>=0 && col>=0){
+            if(curr[row][col]=='Q') return false;
+            row--;
+            col--;
         }
+        
+        row= drow;
+        col = dcol;
 
-        // check upper-left diagonal
-        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q') return false;
+        while(col>=0){
+            if(curr[row][col]=='Q') return false;
+            col--;
         }
-
-        // check lower-left diagonal
-        for (int i = row, j = col; i < n && j >= 0; i++, j--) {
-            if (board[i][j] == 'Q') return false;
+        row= drow;
+        col = dcol;
+        while(row<curr.size() && col>=0){
+            if(curr[row][col]=='Q') return false;
+            row++;
+            col--;
         }
-
         return true;
+
     }
-
-    void f(int col, vector<vector<string>>& ans, vector<string>& temp, int n) {
-        if (col == n) {
-            ans.push_back(temp);
-            return; // IMPORTANT
+    void f(int col, vector<vector<string>>&ans ,vector<string>&curr, int n){
+        if(col==n){
+            ans.push_back(curr);
+            return;
         }
-
-        for (int row = 0; row < n; row++) {
-            if (isSafe(row, col, temp)) {
-                temp[row][col] = 'Q';
-                f(col + 1, ans, temp, n);
-                temp[row][col] = '.'; 
+        for(int row=0;row<n;row++){
+            if(isSafe(row , col , curr)){
+                curr[row][col]='Q';
+                f(col+1 ,ans, curr, n);
+                curr[row][col]='.';
             }
         }
-    }
 
+    }
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
-        vector<string> temp(n, string(n, '.'));
-
-        f(0, ans, temp, n);
+        vector<string> curr(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++){
+            curr[i]=s;
+        }
+        f(0,  ans, curr, n);
         return ans;
     }
 };
